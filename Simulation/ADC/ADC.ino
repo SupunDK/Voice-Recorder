91a98b1bdd97b665efbbb69b6fb10b3e1095b265
Supun <supun2110@gmail.com>
2021-04-02 20:00:18 +0530

    Encoded the quantized levels

--- Simulation/ADC/ADC.ino
+++ Simulation/ADC/ADC.ino
@@ -1,14 +1,37 @@
 const int analogReadPin = 0;
-int val;
+int sample;
+String b_sample;
+int start;
+
+String DtoB(int d_val);
 
 void setup(){
-    Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop(){
-    Serial.println("\nTime: ");
-    Serial.println(millis());
+  start = millis();
+  sample = map(analogRead(analogReadPin), 0, 1023, 0, 255);
+  
+  b_sample = DtoB(sample);
 
-    val = map(analogRead(analogReadPin), 0, 1023, 0, 255);
+  Serial.print(sample);
+  Serial.print(" => ");
+  Serial.println(b_sample);
+}
 
+String DtoB(int d_val){
+    String b_val = "";
+
+    while(d_val != 0){
+        b_val = String(d_val%2) + b_val;
+
+        d_val = d_val/2;
+    }
+
+    while(b_val.length() != 8){
+        b_val = "0" + b_val;
+    }
+
+    return b_val;
 }
