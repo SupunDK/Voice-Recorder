c5205a14f50af26e7fa7a1b3e28ef0a59b86d8bf
Supun <supun2110@gmail.com>
2021-04-08 14:43:36 +0530

    Added the equaliser simulation

--- Simulation/Voice Manipulations/conv_test.m
+++ Simulation/Voice Manipulations/conv_test.m
@@ -1,6 +1,6 @@
 Fs = 1000;
 Ts = 1/Fs;
-t = 0:Ts:5;
+t = -10:Ts:10;
 
 l = length(t);
 
@@ -8,13 +8,18 @@ y = sin(2*pi*50*t);
 
 x = sin(2*pi*10*t) ./ (pi*t);
 
-%z = conv(y, x);
+figure;
+hold on;
+stem(t, x);
 
+z = conv(y, x);
+
+figure;
 subplot(3,1,1);
-show_freq(y, l, Fs, "y");
+show_freq_equaliser(y, l, Fs, "y");
 
 subplot(3,1,2);
-show_freq(x, l, Fs, "x");
+show_freq_equaliser(x, l, Fs, "x");
 
 %subplot(3,1,3);
 %show_freq(z(1:5001), l, Fs, "z");
--- Simulation/Voice Manipulations/frequency_shifting_on_music.m
+++ Simulation/Voice Manipulations/frequency_shifting_on_music.m
@@ -3,4 +3,4 @@ file = "africa-toto.wav";
 [y, Fs] = audioread(file, [20*22050, 30*22050]);
 
 figure;
-show_freq(y, length(y), Fs);
+show_freq_equaliser(y, length(y), Fs);
